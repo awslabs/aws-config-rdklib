ca79083849d6257605bd2cd74ab3742395ae7229
rickychau2780 <chauyu@amazon.com>
2020-04-07 20:34:24 -0400

    0.1.0 release

--- rdklib/__init__.py
+++ rdklib/__init__.py
@@ -6,7 +6,7 @@
 #
 #    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-MY_VERSION = "0.0.3"
+MY_VERSION = "0.1.0"
 
 from .configrule import ConfigRule, MissingTriggerHandlerError
 from .evaluator import Evaluator
